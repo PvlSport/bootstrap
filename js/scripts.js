60483b3813a9ae901ae0c633113cd3acd568115e
torekhan <torekhan93@mail.ru>
2017-10-21 20:47:21 +0100

    NPM scripts

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $("[data-toggle='tooltip']").tooltip();
+});
+
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    
+    // $("#carousel-pause").click(function(){
+    //     $("#mycarousel").carousel('pause');
+    // });
+    // $("#carousel-play").click(function(){
+    //     $("#mycarousel").carousel('cycle');
+    // });
+
+    $("#carousel-button").click(function(){
+        if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if ($("#carousel-button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');                    
+        }
+    });
+});
